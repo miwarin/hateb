80b9b4afc814e1f61fc81fb2e9c7bdbd9ab0a95f
Susumu Miwa <miwarin@gmail.com>
2014-02-23 23:12:47 +0900

    create

---
+++ hateb/frmMain.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace hateb2
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtAPIKey = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.btnHateb = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(12, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 12);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "API KEY";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(11, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "ID";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtInput.Location = new System.Drawing.Point(12, 61);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtInput.Size = new System.Drawing.Size(400, 272);
+            this.txtInput.TabIndex = 9;
+            // 
+            // txtAPIKey
+            // 
+            this.txtAPIKey.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtAPIKey.Location = new System.Drawing.Point(75, 37);
+            this.txtAPIKey.Name = "txtAPIKey";
+            this.txtAPIKey.Size = new System.Drawing.Size(268, 19);
+            this.txtAPIKey.TabIndex = 8;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txtUsername.Location = new System.Drawing.Point(75, 12);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(268, 19);
+            this.txtUsername.TabIndex = 7;
+            // 
+            // btnHateb
+            // 
+            this.btnHateb.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnHateb.Location = new System.Drawing.Point(349, 22);
+            this.btnHateb.Name = "btnHateb";
+            this.btnHateb.Size = new System.Drawing.Size(63, 33);
+            this.btnHateb.TabIndex = 6;
+            this.btnHateb.Text = "はてブ";
+            this.btnHateb.UseVisualStyleBackColor = true;
+            this.btnHateb.Click += new System.EventHandler(this.btnHateb_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 347);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.txtAPIKey);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.btnHateb);
+            this.Name = "frmMain";
+            this.Text = "はてブ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtAPIKey;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Button btnHateb;
+    }
+}
+
---
+++ hateb/frmMain.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Net.Http;
+using System.Net;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Net;
+using System.IO;
+
+namespace hateb2
+{
+
+    public partial class frmMain : Form
+    {
+        struct PostURI
+        {
+            public String uri;
+            public String comment;
+        }
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHateb_Click(object sender, EventArgs e)
+        {
+            String username;
+            String api_key;
+            ArrayList uris;
+            String postURI = "http://b.hatena.ne.jp/atom/post";
+
+            username = getUsername();
+            api_key = getApikey();
+            uris = getURI();
+
+            var client = new HttpClient(new WsseClientHandler { UserName = username, Password = api_key });
+
+            foreach (PostURI uri in uris)
+            {
+                byte[] content;
+                content = getContent(uri.uri, uri.comment);
+                ByteArrayContent h = new ByteArrayContent(content);
+                client.PostAsync(postURI, h);
+            }
+
+        }
+        private String getUsername()
+        {
+            String username = txtUsername.Text;
+            return username;
+        }
+
+        private String getApikey()
+        {
+            String apikey = txtAPIKey.Text;
+            return apikey;
+        }
+
+        private ArrayList getURI()
+        {
+            ArrayList uris = new ArrayList();
+            String text = txtInput.Text;
+            String[] sp = text.Split(new String[] { "\r\n" }, StringSplitOptions.None);
+            int count = sp.Count();
+            int i;
+
+            for (i = 0; i < count; )
+            {
+                if (sp[i] == "")
+                {
+                    break;
+                }
+                PostURI pu = new PostURI();
+                pu.uri = sp[i];
+                pu.comment = sp[i + 1];
+                uris.Add(pu);
+                i += 2;
+            }
+            return uris;
+        }
+
+        // タイトルはダミー
+        // はてなのAPIをつかってみた / Rails4とWSSE認証 | Workabroad.jp http://www.workabroad.jp/posts/2040
+        private byte[] getContent(String uri, String comment)
+        {
+
+            String entry = String.Format("<entry xmlns=\"http://purl.org/atom/ns#\"><title>dummy</title><link rel=\"related\" type=\"text/html\" href=\"{0} \" /><summary type=\"text/plain\">{1}</summary></entry>", uri, comment);
+
+            byte[] data = Encoding.UTF8.GetBytes(entry);
+            return data;
+        }
+    
+    }
+    
+    // HttpClientでWSSEしてみる - tmytのらくがき http://tmyt.hateblo.jp/entry/20130320/1363789847
+    class WsseClientHandler : HttpClientHandler
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        private string GenerateCredentials()
+        {
+            var created = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ");
+            var nonce = WebUtility.UrlEncode(Guid.NewGuid().ToString());
+            using (var sha1 = SHA1.Create())
+            {
+                var bytes = Encoding.UTF8.GetBytes(nonce + created + Password);
+                var digest = Convert.ToBase64String(sha1.ComputeHash(bytes));
+                return string.Format(@"UsernameToken Username=""{0}"", PasswordDigest=""{1}"", Nonce=""{2}"", Created=""{3}""",
+                UserName, digest, Convert.ToBase64String(Encoding.UTF8.GetBytes(nonce)), created);
+            }
+        }
+
+        protected override System.Threading.Tasks.Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+        {
+            request.Headers.Add("X-WSSE", GenerateCredentials());
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
